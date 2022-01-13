53f176b96933c5bc6a75ac9ba31a2378f21e85e9
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-01-13 16:24:18 +0000

    Resubmission with input variabes

--- modules/input.py
+++ modules/input.py
@@ -1,3 +1,7 @@
 
+option_4_input_text = "Enter task description to search for: "
+option_5_input_text = "Enter task duration: "
+option_6_input_text = "Enter task description to search for: "
+option_7_description_input_text = "Enter description: "
+option_7_time_taken_input_text = "Enter time taken: "
 
-option_four_input = "Enter task description to search for: "
