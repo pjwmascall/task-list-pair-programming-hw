092b86c2f32da65397c0ed59f56b5a618ad176b0
Paul Mascall <pjwmascall@hotmail.com>
2022-01-13 20:07:06 +0000

    bugs fixed

--- app.py
+++ app.py
@@ -5,26 +5,27 @@ from modules.input import *
 
 while (True):
     print_menu()
-    if (option().lower() == 'q'):
+    option = select_option()
+    if (option.lower() == 'q'):
         break
-    if option() == '1':
+    elif option == '1':
         print_list(tasks)
-    elif option() == '2':
+    elif option == '2':
         print_list(get_uncompleted_tasks(tasks))
-    elif option() == '3':
+    elif option == '3':
         print_list(get_completed_tasks(tasks))
-    elif option() == '4':
+    elif option == '4':
         task = get_task_with_description(tasks, option_4_input())
         if task is not None:
             mark_task_complete(task)
             print("Task marked complete")
         else:
             print("Task not found")
-    elif option() == '5':
+    elif option == '5':
         print_list(get_tasks_taking_at_least(tasks, option_5_input()))
-    elif option() == '6':
+    elif option == '6':
         print(get_task_with_description(tasks, option_6_input()))
-    elif option() == '7':
+    elif option == '7':
         task = create_task(option_7_input_description(), option_7_input_time_taken())
         tasks.append(task)
     else:
--- modules/input.py
+++ modules/input.py
@@ -1,5 +1,6 @@
-def option():
-    return input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
+
+def select_option():
+    return input("Select an option 1, 2, 3, 4, 5, 6, 7, display (m)enu or (q)uit: ")
 def option_4_input():
     return input("Enter task description to search for: ")
 def option_5_input():
